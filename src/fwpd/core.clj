d6bb784861a6e594074501c83114ce9b67b065d5
André Martins <andresmartins910@gmail.com>
2023-07-08 11:25:45 -0300

    done

--- src/fwpd/core.clj
+++ src/fwpd/core.clj
@@ -1,7 +1,66 @@
 (ns fwpd.core
-  (:gen-class))
+  (:require [clojure.string]
+            [clojure.java.io]
+            [clojure.data.csv :as csv]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def filename "suspects.csv")
+
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int [str]
+  (Integer. str))
+
+(def conversions {:name identity :glitter-index str->int})
+
+(defn convert [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(convert :glitter-index "10")
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",") (clojure.string/split string #"\n")))
+
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+(def suspects (mapify (parse (slurp filename))))
+
+(defn map->csv-str [coll]
+  (->> coll
+       (map vals)
+       (map (partial clojure.string/join ","))
+       (clojure.string/join "\n")))
+
+(map->csv-str suspects)
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (into '() (map :name (filter #(>= (:glitter-index %) minimum-glitter) records))))
+
+(glitter-filter 3 (mapify (parse (slurp filename))))
+
+(defn int->str [int]
+  (str int))
+
+(defn validate [{:keys [name glitter-index]}]
+  (when (and name glitter-index)
+    [name (int->str glitter-index)]))
+
+(def not-empty? (complement empty?))
+
+(defn append-to-csv [record]
+  (let [new-row (validate record)]
+    (when (not-empty? new-row)
+      (with-open [file-path (clojure.java.io/writer "suspects.csv" :append true)]
+        (csv/write-csv file-path [new-row])))))
+
+
